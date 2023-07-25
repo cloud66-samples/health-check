f7c428c711b0909323bfcc904bd1767b9e16fbe5
DimitriosLisenko <DimitriosLisenko@users.noreply.github.com>
2023-07-25 13:42:18 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'health', action: :health, controller: 'health'
   get 'healthz', action: :healthz, controller: 'health'
+  get 'sleepy_health', action: :sleepy_health, controller: 'health'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
